13daa877d8950b36827381f2cdce3ab0492610fe
mecha-2 <m.castelo122@gmail.com>
2025-12-15 22:00:32 -0300

    Delete ejerclase8 directory

--- ejerclase8/ejercicio5.js
+++
@@ -1,7 +0,0 @@
-const prompt = require ('prompt-sync')();
-
-//pedimos al usuarioque ingrese su nombre 
-let nombre = prompt ("¿Cómo te llamas?");
-
-//mostramos el saludo personalizado
-console.log("Hola, " + nombre + " ¿Que onda?.");
